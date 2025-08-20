5a1e9201cd49695e740a7276713db5ebe4718cc3
Reossann <s1f102502283@iniad.org>
2025-08-20 20:45:38 +0900

    なんかうまくいかない

--- app.ts
+++ app.ts
@@ -1,11 +1,29 @@
-function handleKeyDown(e: KeyboardEvent): void{
-    const audio = document.querySelector<HTMLAudioElement>(`audio[data-key="${e.key}"]`);
-    const key = document.querySelector<HTMLDivElement>(`div[data-key="${e.key}"]`);
-    if (!audio || !key){return}
-    key.classList.add('playing');
-    audio.currentTime = 0;
-    audio.play()
-;
+// --- ここからコピー ---
+
+// キーが押された時に実行される関数
+function playSound(e: KeyboardEvent): void {
+  const audio = document.querySelector<HTMLAudioElement>(`audio[data-key="${e.key}"]`);
+  const key = document.querySelector<HTMLDivElement>(`div[data-key="${e.key}"]`);
+
+  if (!audio || !key) return;
+
+  key.classList.add('playing');
+  audio.currentTime = 0;
+  audio.play();
 }
 
-window.addEventListener("keydown", handleKeyDown)
+// アニメーションが終わった時に実行される関数
+function removeTransition(e: TransitionEvent): void {
+  if (e.propertyName !== 'transform') return;
+  (e.target as HTMLElement).classList.remove('playing');
+}
+
+// 1. すべての.key要素を取得
+const keys = document.querySelectorAll<HTMLDivElement>('.key');
+// 2. それぞれのキーに、transitionendイベントの監視を設定
+keys.forEach(key => key.addEventListener('transitionend', removeTransition as EventListener));
+
+// ページ全体でキーボードが押されるのを監視
+window.addEventListener('keydown', playSound);
+
+// --- ここまでコピー ---
